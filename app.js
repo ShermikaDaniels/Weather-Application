b77a9289307411514bd58196c9e9ff82db5eacde
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2022-10-21 18:57:45 -0400

    showing the current temperature

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let forcastElement = document.querySelector("#forecast");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  forcastElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "cea86aeaf9414de0b74t0d5132af8ebo";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=New York&key=cea86aeaf9414de0b74t0d5132af8ebo&units=metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=New York&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
