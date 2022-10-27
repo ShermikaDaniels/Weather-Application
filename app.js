94cb61f5ca3420575da30662d849d5afe4f4abf5
ShermikaDaniels <112131028+ShermikaDaniels@users.noreply.github.com>
2022-10-27 19:41:36 -0400

    Edited the forecastElement function

--- app.js
+++ app.js
@@ -40,8 +40,7 @@ function forecastElement() {
             <span class="weather-forecast-temperature-max">18°</span> 
             <span class="weather-forecast-temperature-min">12°</span>
             </div>
-          </div>
-        </div>`;
+          </div>`;
   });
 
   forecastHTML = forecastHTML + `</div>`;
